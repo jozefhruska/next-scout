f241b3006a47387ca283e96a8cc2c30174a3b6da
jozefhruska <me@jozefhruska.com>
2022-12-02 17:06:32 +0100

    fix: exclude _error and _middleware files

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,12 @@ export const ROOT_NODE_NAME = 'ROOT';
 export const DEFAULT_APP_FOLDER_PATH = './app/';
 export const DEFAULT_PAGES_FOLDER_PATH = './pages/';
 
-export const EXCLUDED_FILES = ['_app', '_document'];
+// Excluded files directly in the "./pages" directory
+export const EXCLUDED_FILES_PAGES_ROOT = ['_app', '_document', '_error'];
+
+// Excluded files anywhere in the "./pages" subtree.
+// Fixes an issue with older versions of Next.js where
+// middleware could be placed anywhere in the "./pages" subtree
+export const EXCLUDED_FILES_ANYWHERE = ['_middleware'];
+
 export const EXCLUDED_FOLDERS = ['api'];
