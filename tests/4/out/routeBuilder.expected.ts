b66a14900526b683d8019d5ede6d426321275cf5
jozefhruska <me@jozefhruska.com>
2022-12-13 11:38:18 +0100

    test: add integration tests for trailing slash

---
+++ tests/4/out/routeBuilder.expected.ts
@@ -0,0 +1,93 @@
+export type RouteBuilder = {
+  blog: {
+    slug: {
+      getPath: (slug: string) => string;
+    },
+  },
+  dashboard: {
+    analytics: {
+      getPath: () => string;
+    },
+    settings: {
+      password: {
+        getPath: () => string;
+      },
+      profile: {
+        getPath: () => string;
+      },
+    },
+    uploads: {
+      rest: {
+        getPath: (rest: string[]) => string;
+      },
+    },
+  },
+  getPath: () => string;
+  contactUs: {
+    getPath: () => string;
+  },
+  projects: {
+    pid: {
+      rest: {
+        getPath: (pid: string, rest: string[]) => string;
+      },
+    },
+  },
+  services: {
+    slug: {
+      getPath: (slug: string) => string;
+    },
+  },
+  test: {
+    rest: {
+      getPath: (rest: string[]) => string;
+    },
+  },
+};
+
+export const routeBuilder: RouteBuilder = {
+  blog: {
+    slug: {
+      getPath: (slug) => `/blog/${slug}/`,
+    },
+  },
+  dashboard: {
+    analytics: {
+      getPath: () => `/dashboard/analytics/`,
+    },
+    settings: {
+      password: {
+        getPath: () => `/dashboard/settings/password/`,
+      },
+      profile: {
+        getPath: () => `/dashboard/settings/profile/`,
+      },
+    },
+    uploads: {
+      rest: {
+        getPath: (rest) => `/dashboard/uploads/${rest.join('/')}/`,
+      },
+    },
+  },
+  getPath: () => `/`,
+  contactUs: {
+    getPath: () => `/contact-us/`,
+  },
+  projects: {
+    pid: {
+      rest: {
+        getPath: (pid, rest) => `/projects/${pid}/${rest.join('/')}/`,
+      },
+    },
+  },
+  services: {
+    slug: {
+      getPath: (slug) => `/services/${slug}/`,
+    },
+  },
+  test: {
+    rest: {
+      getPath: (rest) => `/test/${rest.join('/')}/`,
+    },
+  },
+};
--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 
 tests_dir=$(dirname -- "$0")
 
-for i in $(seq 1 3); do
+for i in $(seq 1 4); do
   (
     cd "$tests_dir/$i/" || exit 1
     bash ./run.sh
